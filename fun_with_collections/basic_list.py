ba6a249f22a7b7eadd98c08b1f9f81a3b7dc71f0
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-03-04 13:43:30 -0600

    getting there

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,10 +1,16 @@
 
 #  return a list of user input
 def make_list()
-    pass
+    while count < 3
+        x = get_input()
+        count += 1
+        y = int(x)
+        y.append(y)
 #  get one input and return it
 def get_input()
-    pass
+    user_input = input('Please enter an input: ')
+
+
 
 if __name__ == '__main__':
     make_list()
